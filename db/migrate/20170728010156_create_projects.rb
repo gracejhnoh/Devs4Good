dada51930b9f0267391426f5a3cca1766b4dcc11
Cassi Gallagher & Grace Noh <pair+cassig.gracenoh153@devbootcamp.com>
2017-07-27 18:23:32 -0700

    Create project index

---
+++ db/migrate/20170728010156_create_projects.rb
@@ -0,0 +1,12 @@
+class CreateProjects < ActiveRecord::Migration[5.1]
+  def change
+    create_table :projects do |t|
+      t.integer :organization_id
+      t.text :description
+      t.date :time_frame
+      t.string :title
+      
+      t.timestamps
+    end
+  end
+end
