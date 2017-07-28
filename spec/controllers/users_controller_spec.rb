65ec9f0a344865cacea0dc255a9c7418cc2f7fb2
Jarred Taylor & Jorge Contreras <pair+jarredt.jcont12@devbootcamp.com>
2017-07-28 11:00:03 -0700

    Create developer tests on user controller spec file

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -15,20 +15,84 @@ describe UsersController do
   end
 
   describe "post developers" do
-    before(:each) do
-      post :create, params: { user: {first_name: Faker::Name.first_name,
-      last_name: Faker::Name.last_name,
-      user_type: 'dev',
-      email: Faker::Internet.safe_email,
-      password: Faker::Internet.password,
-      website: Faker::Internet.domain_name,
-      description: Faker::StarWars.quote,
-      phone: Faker::PhoneNumber.cell_phone
+    context "valid params" do
+      before(:each) do
+        post :create, params: { user: {first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        user_type: 'dev',
+        email: Faker::Internet.safe_email,
+        password: Faker::Internet.password,
+        website: Faker::Internet.domain_name,
+        description: Faker::StarWars.quote,
+        phone: Faker::PhoneNumber.cell_phone
+        }
       }
-    }
+      end
+      it "responds 302" do
+        expect(response).to have_http_status(302)
+      end
+
+      it "creates a new developer in the database" do
+        expect{post :create, params: { user: {first_name: Faker::Name.first_name,
+          last_name: Faker::Name.last_name,
+          user_type: 'dev',
+          email: Faker::Internet.safe_email,
+          password: Faker::Internet.password,
+          website: Faker::Internet.domain_name,
+          description: Faker::StarWars.quote,
+          phone: Faker::PhoneNumber.cell_phone
+          }
+         }
+        }.to change{User.where(user_type:'dev').count}.by(1)
+      end
+
+      it "assigns the newly created developer as @developer" do
+        expect(assigns[:user]).to eq User.last
+      end
+
+      it "redirects to root" do
+        expect(response).to redirect_to root_path
+      end
     end
-    it "responds 302" do
-      expect(response).to have_http_status(302)
+    context "invalid params" do
+      before(:each) do
+        post :create, params: { user: {first_name: '',
+          last_name: Faker::Name.last_name,
+          user_type: 'dev',
+          email: Faker::Internet.safe_email,
+          password: Faker::Internet.password,
+          website: Faker::Internet.domain_name,
+          description: Faker::StarWars.quote,
+          phone: Faker::PhoneNumber.cell_phone
+          }
+        }
+      end
+
+      it "responds with status 422" do
+        expect(response).to have_http_status(422)
+      end
+
+      it "does not create a new developer in database" do
+        expect{post :create, params: { user: {first_name: '',
+          last_name: Faker::Name.last_name,
+          user_type: 'dev',
+          email: Faker::Internet.safe_email,
+          password: Faker::Internet.password,
+          website: Faker::Internet.domain_name,
+          description: Faker::StarWars.quote,
+          phone: Faker::PhoneNumber.cell_phone
+          }
+         }
+       }.not_to change{User.where(user_type:'dev').count}
+      end
+
+      it "assigns the unsaved developer as @user" do
+        expect(assigns[:user]).to be_a_new(User)
+      end
+
+      it "renders the new template" do
+        expect(response).to render_template :new
+      end
     end
   end
 end
