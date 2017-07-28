4902b37dffd935d303910707de0d2ffeb51d2948
Jorge Contreras <jorgeacontrerasl@gmail.com>
2017-07-27 20:12:32 -0700

    Correct user_type field name, finalize users#create route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save!
+      redirect_to :root
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :org_name, :street_address, :city, :state, :zip, :phone, :website, :description, :user_type, :email, :password)
   end
 
 end
