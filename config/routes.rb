6dd80eddbe0c75637c931f7b14ee01d0662a32f1
Cassi Gallagher & Grace Noh <pair+cassig.gracenoh153@devbootcamp.com>
2017-07-27 19:48:13 -0700

    Create Project Show Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :projects, :only => [:index]
-  
+  resources :projects, :only => [:index, :show]
+
   get "home/index"
   root :to => 'home#index'
 end
