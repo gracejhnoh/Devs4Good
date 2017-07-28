837ca79f2a52e7d5dffc7a5e633d492d05fc521a
Cassi Gallagher & Grace Noh <pair+cassig.gracenoh153@devbootcamp.com>
2017-07-27 19:29:29 -0700

    create seed data for project view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Project.delete_all
+
+10.times do
+  Project.create(organization_id: Faker::Number.digit, description: Faker::Hipster.paragraph, time_frame: Faker::Date.forward(30), title: Faker::HarryPotter.quote)
+end
