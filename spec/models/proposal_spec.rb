735eb23822c414ccaf9c0c05703002d8e8ea0125
Grace Noh & Jorge Contreras <pair+gracenoh153.jcont12@devbootcamp.com>
2017-07-29 11:02:53 -0700

    rspec tests for Proposal Model

--- spec/models/proposal_spec.rb
+++ spec/models/proposal_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe Proposal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Proposal do
+  context 'validations' do
+    it { is_expected.to validate_presence_of :user_id }
+
+    it { is_expected.to validate_presence_of :project_id }
+
+    it { is_expected.to validate_presence_of :description }
+  end
 end
