eeb9d4b107efd0ffd9f2c7e8457772b6d33fef43
Cassi Gallagher & Grace Noh <pair+cassig.gracenoh153@devbootcamp.com>
2017-07-28 19:50:12 -0700

    temporary commit; work on these later

---
+++ spec/controllers/proposals_controller_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe ProposalsController, type: :controller do
+
+  describe 'POST #create' do
+    let!(:user) { FactoryGirl.create(:developer) }
+    let!(:test_project) { FactoryGirl.create(:project) }
+  
+    it 'returns a status code of 200' do
+      post :create, params: { project_id: test_project.id, proposal: FactoryGirl.attributes_for(:proposal) }
+      expect(response.status).to eq 200
+    end
+
+    context 'with valid attributes' do
+      it 'creates a new proposal' do
+        expect {
+          post :create, params: { project_id: test_project.id, proposal: FactoryGirl.attributes_for(:proposal)
+        } }.to change(Proposal, :count).by 1
+      end
+
+      it 'returns a status code of 302' do
+        post :create,  params: { proposal: FactoryGirl.attributes_for(:proposal) }
+        expect(response).to have_http_status 302
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not create a new proposal' do
+        expect {
+          post :create, params: { proposal: { description: nil } } }.not_to change{ Proposal.all.count }
+      end
+
+      it 'renders the new proposal view' do
+        post :create, params: { proposal: { description: nil } }
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
+end
--- spec/factories/proposals.rb
+++ spec/factories/proposals.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :proposal do
-    project nil
-    developer nil
-    description "MyText"
+    project Faker::Number.digit
+    developer Faker::Number.digit
+    description Faker::Hipster.paragraph
     selected? false
   end
 end
