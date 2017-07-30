128105b116846388a0fea41b8b9347c9a8fa7167
Grace Noh & Jorge Contreras <pair+gracenoh153.jcont12@devbootcamp.com>
2017-07-29 17:29:59 -0700

    add tests for selected proposal method for project

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -10,4 +10,20 @@ describe Project do
     it { is_expected.to validate_presence_of :time_frame }
   end
 
+  context 'selected proposal' do
+    let!(:developer) { FactoryGirl.create(:developer) }
+    let!(:organization) { FactoryGirl.create(:organization) }
+    let!(:project) { FactoryGirl.create(:project) }
+    let!(:proposal) { FactoryGirl.create(:proposal, project: project, developer: developer) }
+
+    it 'returns nil if no proposal selected' do
+      expect(project.selected_proposal).to be_nil
+    end
+
+    it 'returns the selected project' do
+      proposal.selected = true
+      proposal.save
+      expect(project.selected_proposal).to eq proposal
+    end
+  end
 end
