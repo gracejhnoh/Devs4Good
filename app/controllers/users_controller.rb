78f07412b110df45448c8710b1e37100f12bb7da
Grace Noh & Jorge Contreras <pair+gracenoh153.jcont12@devbootcamp.com>
2017-07-31 14:50:20 -0700

    add working edit view page for developers and organizations; create edit and update methods on users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,6 +32,25 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    if (params[:id].to_i != current_user.id)
+      redirect_to root_path
+    else
+      @user = User.find(params[:id])
+      if @user.update(
+        user_params
+        )
+        if @user.user_type == 'dev'
+          redirect_to developer_path
+        else
+          redirect_to organization_path
+        end
+      else
+        render :edit
+      end
+    end
+  end
+
   private
 
   def user_params
