e268124c93960f0184919974feb6c91b8517bada
Jarred Taylor <jarredt@gmail.com>
2017-09-11 13:03:40 -0700

    Reorganize routes.rb, remove #index from nested projects resource, delete extraneous get home/index specification

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,24 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :projects, only: :index do
+  root :to => 'home#index'
+
+  resources :projects, only: [:index] do
     resources :proposals, except: [:index]
   end
 
+  resources :users, only: [:new]
+
   resources :users, path: :organizations, only: [:show, :new, :create, :edit, :update], as: 'organizations' do
-    resources :projects
+    resources :projects, except: [:index]
   end
+
   resources :users, path: :developers, only: [:show, :new, :create, :edit, :update], as: 'developers'
 
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:new]
 
   get 'pages/about' => 'pages#about'
 
   get 'login' => 'sessions#new', :as => :login
   post 'logout' => 'sessions#destroy', :as => :logout
-  get "home/index"
-  root :to => 'home#index'
+
 end
