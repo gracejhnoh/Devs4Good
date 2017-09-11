fa7a0150bdc5cf7f82cd9f45046098f61e28bcad
Jarred Taylor <jarredt@gmail.com>
2017-09-11 14:52:06 -0700

    Add numerous view helpers and implement in Projects/show view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def is_dev?
+    current_user.user_type == 'dev'
+  end
+
+  def is_project_org?
+    @project.organization.id == current_user.id
+  end
+
+  def has_contact_email?
+    @project.contact_email != ''
+  end
 end
