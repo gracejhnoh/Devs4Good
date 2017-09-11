c0d820ba5d7f6a16f42d20c30e52b9a06252427b
Jarred Taylor <jarredt@gmail.com>
2017-09-11 12:37:31 -0700

    Reorganize ordering/spacing of User model associations and validations for clarity and pattern consistency

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   dragonfly_accessor :image
   has_many :proposals
-
+  has_many :projects, foreign_key: :organization_id
   authenticates_with_sorcery!
-  validates :password, presence: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/ }
 
+  validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/ }
+  validates :password, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
@@ -19,10 +19,10 @@ class User < ApplicationRecord
                           if: Proc.new { |u| u.user_type === 'org'}
   validates :website, format: { with: /\A(http:\/\/|https:\/\/)/ , message: "must include http:// or https://" }, allow_blank: true
   validates :phone, format: { with: /\A\d{3}-\d{3}-\d{4}\z/ , message: "must be in XXX-XXX-XXXX format"}, allow_blank: true
-  validates :ein, format: { with: /\A\d{9}\z/, message: "must be 9 digits (without hyphen)"}, allow_blank: true
-  validates_property :format, of: :image, in: [:jpeg, :jpg, :png, :bmp], case_sensitive: false, message: 'must be .jpeg, .jpg, .png, .bmp format', if: :image_changed?
 
-  has_many :projects, foreign_key: :organization_id
+  validates :ein, format: { with: /\A\d{9}\z/, message: "must be 9 digits (without hyphen)"}, allow_blank: true
+
+  validates_property :format, of: :image, in: [:jpeg, :jpg, :png, :bmp], case_sensitive: false, message: 'must be .jpeg, .jpg, .png, .bmp format', if: :image_changed?
 
   def full_name
     self.first_name + ' ' + self.last_name
